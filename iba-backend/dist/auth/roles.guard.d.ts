5f4cd22fc6f41a576faf8ba57e7d60802f3283a2
Abdul Wasay Imran <abdulwasay905@gmail.com>
2026-04-11 04:16:56 +0500

    added dist to gitignore too for cleaner repo

--- iba-backend/dist/auth/roles.guard.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const Roles: (...roles: string[]) => import("@nestjs/common").CustomDecorator<string>;
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-export declare class RolesGuard implements CanActivate {
-    private reflector;
-    constructor(reflector: Reflector);
-    canActivate(ctx: ExecutionContext): boolean;
-}
--- iba-backend/dist/supabase/supabase.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { SupabaseClient } from '@supabase/supabase-js';
-export declare class SupabaseService {
-    private config;
-    private client;
-    constructor(config: ConfigService);
-    get db(): SupabaseClient;
-}
